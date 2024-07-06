2895a8750371f83b40da8ba1496a7b05b26d04eb
JakMar17 <jakob.marusic17@gmail.com>
2024-07-06 12:20:57 +0200

    feat: mobile navbar

--- components/navigation/navigation-header.component.vue
+++ components/navigation/navigation-header.component.vue
@@ -3,14 +3,14 @@
     <NuxtLink to="/" class="title mb-0" style="font-weight: 800; font-size: 32px; flex-grow: 1">
       JM
     </NuxtLink>
-    <div class="nav__wrapper">
+    <div class="nav__wrapper is-hidden-touch">
       <div class="is-flex nav__buttons">
         <NuxtLink to="projekti" class="button is-white is-rounded" :class="{'link--active': isActive('projekti')}">Projekti</NuxtLink>
         <NuxtLink to="cv" class="button is-white is-rounded" :class="{'link--active': isActive('cv')}">Izkušnje</NuxtLink>
         <NuxtLink to="jaz" class="button is-white is-rounded" :class="{'link--active': isActive('jaz')}">O meni</NuxtLink>
       </div>
     </div>
-    <div class="is-flex is-align-items-center is-justify-content-flex-end social__wrapper" >
+    <div class="is-flex is-align-items-center is-justify-content-flex-end social__wrapper is-hidden-touch">
       <a href="https://github.com/jakmar17" target="_blank" class="button__rounded is-dark">
         <i class="ri-github-fill"></i>
       </a>
@@ -24,14 +24,43 @@
         <i class="ri-facebook-fill"></i>
       </a>
     </div>
+
+    <div class="is-hidden-desktop">
+      <a @click="onMobileMenuToggle()" class="button__rounded is-dark">
+        <i class="ri-menu-line"></i>
+      </a>
+    </div>
+
+  </div>
+  <div id="mobileMenu" ref="mobileMenu" v-show="isMobileMenuVisible">
+    <div class="is-flex is-justify-content-space-between is-align-items-center" style="min-height: 8em; margin: 0 4em">
+        <NuxtLink to="/" class="title mb-0" style="font-weight: 800; font-size: 32px; flex-grow: 1">
+          JM
+        </NuxtLink>
+        <a @click="onMobileMenuToggle()" class="button__rounded is-dark">
+          <i class="ri-close-line"></i>
+        </a>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 const route = useRoute();
 
+const isMobileMenuVisible = ref(false);
+const mobileMenu = ref(null);
+
+const onMobileMenuToggle = () => {
+  isMobileMenuVisible.value = !isMobileMenuVisible.value;
+  mobileMenu.value.style.opacity = isMobileMenuVisible.value ? 0 : 1;
+  setTimeout(() => {
+    mobileMenu.value.style.opacity = isMobileMenuVisible.value ? 1 : 0;
+  }, 500)
+};
+
+
 const isActive = (linkName: string): boolean =>
-  route.fullPath.includes(linkName);
+    route.fullPath.includes(linkName);
 
 </script>
 
@@ -62,7 +91,8 @@ $box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.125);
 
 .social {
   &__wrapper {
-    gap: 0.55em; flex-grow: 1;
+    gap: 0.55em;
+    flex-grow: 1;
   }
 }
 
@@ -94,5 +124,17 @@ $box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.125);
   &--active {
     background: #f2f2f2;
   }
+}
+
+#mobileMenu {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background: white;
+  z-index: 99;
+  opacity: 0;
+  transition: opacity 0.5s;
 }
 </style>
