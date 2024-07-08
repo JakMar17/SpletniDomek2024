c984e16b8f9eb7dbc6c3731c5eefed6c2495b577
JakMar17 <jakob.marusic17@gmail.com>
2024-07-08 19:37:31 +0200

    feat: added prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,52 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            htmlAttrs: {
-                lang: 'sl-SI',
-            },
-        },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "sl-SI",
+      },
     },
-    css: [
-        "@/assets/styles/main.scss",
-        "@/node_modules/remixicon/fonts/remixicon.css"
-    ],
-    build: {
-        transpile: [],
+  },
+  css: [
+    "@/assets/styles/main.scss",
+    "@/node_modules/remixicon/fonts/remixicon.css",
+  ],
+  build: {
+    transpile: [],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": true,
     },
-    vite: {
-        define: {
-            'process.env.DEBUG': true,
-        },
-        server: {
-            hmr: {
-                port: 3008
-            }
-        },
-        css: {
-            preprocessorOptions: {
-                scss: {}
-            }
-        }
-
+    server: {
+      hmr: {
+        port: 3008,
+      },
     },
-    plugins: [
-    ],
-    modules: ['@nuxt/content', 'v-plausible', 'nuxt-aos', ["@nuxtjs/google-fonts", {
+    css: {
+      preprocessorOptions: {
+        scss: {},
+      },
+    },
+  },
+  plugins: [],
+  modules: [
+    "@nuxt/content",
+    "v-plausible",
+    "nuxt-aos",
+    "@nuxt/eslint",
+    [
+      "@nuxtjs/google-fonts",
+      {
         families: {
-            'Noto Sans JP': [800],
-            'Inter Tight': {
-                wght: '100..900',
-                ital: '100..900'
-            }
-        }
-    }]],
-    plausible: {
-        init: {
-            domain: 'jakob.marela.team',
-            apiHost: 'https://plausible.sven.marela.team/',
-            trackLocalhost: false
+          "Noto Sans JP": [800],
+          "Inter Tight": {
+            wght: "100..900",
+            ital: "100..900",
+          },
         },
-        partytown: false,
-    }
-})
+      },
+    ],
+  ],
+  plausible: {
+    init: {
+      domain: "jakob.marela.team",
+      apiHost: "https://plausible.sven.marela.team/",
+      trackLocalhost: false,
+    },
+    partytown: false,
+  },
+});
